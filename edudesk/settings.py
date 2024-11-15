5dd8d10a8576d77c4006447d21b4c29ff7e947af
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-11-15 11:34:11 +0100

    updating setting.py

--- edudesk/settings.py
+++ edudesk/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['edudesks-3cu3.onrender.com']
+ALLOWED_HOSTS = ['edudesks-3cu3.onrender.com', '127.0.0.1']
 
 
 # Application definition
